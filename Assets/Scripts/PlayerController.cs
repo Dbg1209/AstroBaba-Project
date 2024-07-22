6c30a112a5645656afddeecf56474dbbed1b4e46
Steven Cortes Marroquin <147890496+Stevens1707Cortes@users.noreply.github.com>
2024-07-22 18:18:50 -0500

    Player Movement, and Animations 1 - Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,22 @@ public class PlayerController : MonoBehaviour
     private float moveVertical, moveHorizontal;
     private Rigidbody playerRb;
     private Vector3 movement;
+    private Animator currentAnimator;
+    public bool isAttacking, isGrounded;
+    public bool isDead = false;
+
     void Start()
-    {
+    {   
+        isGrounded = true;
+
         playerRb = GetComponent<Rigidbody>();
+        
      
         model1.SetActive(true);
         model2.SetActive(false);
+        model3.SetActive(false);
+
+        currentAnimator = model1.GetComponent<Animator>();
     }
 
     private void Update()
@@ -26,6 +36,28 @@ public class PlayerController : MonoBehaviour
 
         //Vector Vertical
         movement = transform.forward * moveVertical;
+
+        // Actualizar la animación de movimiento
+        if (currentAnimator != null)
+        {
+            bool isMoving = movement.magnitude > 0.1f;
+            currentAnimator.SetBool("isMoving", isMoving);
+            currentAnimator.SetBool("isAttacking", isAttacking);
+            currentAnimator.SetBool("isDead", isDead);
+        }
+
+        //Atacar con Z
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartAttack();
+        }
+
+        //Saltar con espacio
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
     }
 
     void FixedUpdate()
@@ -33,14 +65,21 @@ public class PlayerController : MonoBehaviour
         playerRb.MovePosition(playerRb.position + movement * speed * Time.fixedDeltaTime);
     }
 
+    //Colisiones
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy")) { 
             //collision.gameObject.SetActive(false);
             AbsorbEnemy(collision.gameObject.name);
         }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 
+    //Mecanica de Absorbcion
     void AbsorbEnemy(string name)
     {
         Debug.Log(name);
@@ -52,16 +91,57 @@ public class PlayerController : MonoBehaviour
         {
             case "Enemy_Capsula":
                 model2.SetActive(true);
+                currentAnimator = model2.GetComponent<Animator>();
                 break;
             case "Enemy_Cube":
                 model3.SetActive(true);
+                currentAnimator = model3.GetComponent<Animator>();
                 break;
             default:
                 Debug.LogWarning("Unknown item type!");
                 break;
         }
+        
+        //Actualizar el isMoving con el nuevo parametro
+        if (currentAnimator != null)
+        {
+            bool isMoving = movement.magnitude > 0.1f;
+            currentAnimator.SetBool("isMoving", isMoving);
+            currentAnimator.SetBool("isAttacking", isAttacking);
+            currentAnimator.SetBool("isDead", isDead);
+        }
     }
 
+    //Mecanica de ataque
+
+    void StartAttack()
+    {
+        if (currentAnimator != null)
+        {
+            isAttacking = true;
+            currentAnimator.SetBool("isAttacking", isAttacking);
+            Invoke("EndAttack", 0.05f); 
+        }
+    }
+
+    void EndAttack()
+    {
+        isAttacking = false;
+        if (currentAnimator != null)
+        {
+            currentAnimator.SetBool("isAttacking", isAttacking);
+        }
+    }
+
+
+    //Saltar
+    void Jump()
+    {
+        playerRb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    //Empujar objetos
     void PushObstacle()
     {
 
