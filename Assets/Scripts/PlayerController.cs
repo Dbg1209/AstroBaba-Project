52a8ec52493ac7ea1358f1e21bb060d4bd777c8e
Steven Cortes Marroquin <147890496+Stevens1707Cortes@users.noreply.github.com>
2024-07-22 16:28:16 -0500

    Absorption Mechanic 0.5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed, rotationSpeed;
+    [SerializeField] private GameObject model1, model2, model3;
     private float moveVertical, moveHorizontal;
     private Rigidbody playerRb;
     private Vector3 movement;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+     
+        model1.SetActive(true);
+        model2.SetActive(false);
     }
 
     private void Update()
@@ -32,16 +36,33 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy")) { 
-            
+            //collision.gameObject.SetActive(false);
+            AbsorbEnemy(collision.gameObject.name);
         }
     }
 
-    void AbsorbEnemy()
+    void AbsorbEnemy(string name)
     {
+        Debug.Log(name);
+        model1.SetActive(false);
+        model2.SetActive(false);
+        model3.SetActive(false);
 
+        switch (name)
+        {
+            case "Enemy_Capsula":
+                model2.SetActive(true);
+                break;
+            case "Enemy_Cube":
+                model3.SetActive(true);
+                break;
+            default:
+                Debug.LogWarning("Unknown item type!");
+                break;
+        }
     }
 
-    void PushEnemy()
+    void PushObstacle()
     {
 
     }
