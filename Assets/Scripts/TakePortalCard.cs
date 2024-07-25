7bc78ddea56d89859cb4669e21113eb23ac1c366
Steven Cortes Marroquin <147890496+Stevens1707Cortes@users.noreply.github.com>
2024-07-25 09:47:58 -0500

    Card fixed

--- Assets/Scripts/TakePortalCard.cs
+++ Assets/Scripts/TakePortalCard.cs
@@ -6,11 +6,19 @@ public class TakePortalCard : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Model2") || other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") /*|| other.gameObject.CompareTag("Model2") || other.gameObject.CompareTag("Model1") || other.gameObject.CompareTag("Model3") || other.gameObject.CompareTag("Model4")*/)
         {
             Destroy(gameObject);
         }
     }
 
-   
+    //private void OnColliderEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player") /*|| other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Model1") || other.gameObject.CompareTag("Model3") || other.gameObject.CompareTag("Model4")*/)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+
 }
